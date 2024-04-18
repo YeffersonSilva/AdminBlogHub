2062a6f59054b30a49cdcea55dc5424dfa5e6e82
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-18 16:28:28 -0300

    feat: create Category model

---
+++ src/categories/Category.js
@@ -0,0 +1,15 @@
+const sequilize = require('sequelize');
+const connection = require('../database/database');
+
+const Category = connection.define('categories', {
+
+    title: {
+        type: sequilize.STRING,
+        allowNull: false
+    },slug: {
+        type: sequilize.STRING,
+        allowNull: false        
+    }
+});
+
+module.exports = Category;
