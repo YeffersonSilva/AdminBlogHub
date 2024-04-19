28af266d5b569fe6bd79cb01111cd9b55289eecf
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-19 15:59:43 -0300

    feat: create EJS templates for administrator and categories

--- src/categories/CategoriesController.js
+++ src/categories/CategoriesController.js
@@ -2,12 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/categories', (req, res) => {
-    res.send('Categories route');
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new');
 })
 
-router.get('/admin/categories/new', (req, res) => {
-    res.send('New category route');
-})
 
 module.exports = router;
