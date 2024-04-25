b095e358ff17ebe8d8e506f2bcd32520a3a1a6bd
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-25 16:14:42 -0300

    feat: create route to save articles

--- src/articles/ArticlesController.js
+++ src/articles/ArticlesController.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const Category = require('./Category');
+const Article = require('./Article');
+const slugify = require('slugify'); 
 
 router.get('/articles', (req, res) => {
     res.send('Categories route');
@@ -13,5 +15,26 @@ router.get('/admin/articles/new', (req, res) => {
     })
 })
 
+router.post('/articles/save', (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const category = req.body.category;
+    if (title != undefined && body != null) {
+        Article.create({
+            title: title,
+            slug: slugify(title),
+            body: body,
+            categoryId: categoryId
+        }).then(() => {
+            res.redirect('/articles');
+        }).catch((error) => {
+            console.error('Error creating article:', error);
+            res.status(500).send('Internal server error');
+        })
+
+    } else {
+        res.redirect('/admin/articles/new');
+    }
+}   )
 
 module.exports = router;
