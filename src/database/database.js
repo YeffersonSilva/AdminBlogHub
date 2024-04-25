d25357342d660b5a6bc3d513a609a4396bbc6934
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-25 16:17:13 -0300

    chore: change database timezone setting

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,8 @@ const sequilize = require('sequelize');
 
 const connection = new sequilize('guiapress', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
