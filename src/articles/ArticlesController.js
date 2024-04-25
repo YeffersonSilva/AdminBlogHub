86697a35a1430bce89cd1db834cc6e25cee49f7a
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-25 16:02:03 -0300

    feat: add category listing to articles

--- src/articles/ArticlesController.js
+++ src/articles/ArticlesController.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
+const Category = require('./Category');
 
 router.get('/articles', (req, res) => {
     res.send('Categories route');
 })
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send('/admin/articles/new')
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {categories: categories});
+    })
 })
+
 
 module.exports = router;
